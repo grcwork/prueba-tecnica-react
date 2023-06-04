71ad54c28c2199663996ac7f6e2b6c1722c1b428
grcwork <grc.work123@gmail.com>
2023-06-04 02:11:10 -0400

    Storybook/react-router/redux integration issues resolved

--- src/stories/BookDetail.stories.tsx
+++ src/stories/BookDetail.stories.tsx
@@ -7,4 +7,13 @@ export default {
   component: BookDetail,
 };
 
-export const AGameofThrones = () => <BookDetail book={books[0]} />;
+export const AGameOfThrones = () => <BookDetail book={books[0]} />;
+export const AClashOfKings = () => <BookDetail book={books[1]} />;
+export const AStormOfSwords = () => <BookDetail book={books[2]} />;
+export const TheHedgeKnight = () => <BookDetail book={books[3]} />;
+export const AFeastForCrows = () => <BookDetail book={books[4]} />;
+export const TheSwornSword = () => <BookDetail book={books[5]} />;
+export const TheMysteryKnight = () => <BookDetail book={books[6]} />;
+export const ADanceWithDragons = () => <BookDetail book={books[7]} />;
+export const ThePrincessAndTheQueen = () => <BookDetail book={books[8]} />;
+export const TheRoguePrince = () => <BookDetail book={books[9]} />;
--- src/stories/BookTable.stories.tsx
+++ src/stories/BookTable.stories.tsx
@@ -1,19 +1,30 @@
+import React from "react";
 import BookTable from "../components/BookTable";
-
+import { withRouter } from "storybook-addon-react-router-v6";
 import books from "./books.json";
+
+import { Provider } from "react-redux";
+import { store } from "../app/store";
+
+const Store = ({ children }: { children: React.ReactNode }) => (
+  <Provider store={store}>{children}</Provider>
+);
 
 export default {
   title: "BookTable",
   component: BookTable,
+  decorators: [withRouter],
 };
 
 export const Table = () => (
-  <BookTable
-    data={books.map((book) => ({
-      id: parseInt(book.url.split("/").slice(-1)[0]),
-      name: book.name,
-      authors: book.authors.join(),
-      isbn: book.isbn,
-    }))}
-  />
+  <Store>
+    <BookTable
+      data={books.map((book) => ({
+        id: parseInt(book.url.split("/").slice(-1)[0]),
+        name: book.name,
+        authors: book.authors.join(),
+        isbn: book.isbn,
+      }))}
+    />
+  </Store>
 );
